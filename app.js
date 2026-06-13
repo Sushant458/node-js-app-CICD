10eb0af07df823f2eea327ee9c74981a9f840aa3
Sushant Yadnik <yadniksushant@gmail.com>
2026-06-13 15:05:36 +0530

    Update response message in app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from jenkins, added webhook, we are from 14 july batch');
+  res.send('Hello everyone this is the deployement of the jekins with the jenkin file');
 });
 
 app.listen(port, () => {
